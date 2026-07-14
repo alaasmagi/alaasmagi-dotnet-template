604ce64535daf78aa73321b1c348d4d673e30a78
Aleksander Laasmägi <alaasmagi@gmail.com>
2026-07-14 16:35:16 +0300

    A small fix in Domain entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -7,5 +7,5 @@ public class AppUser : BaseEntityWithConcurrency
     public string Email { get; set; } = default!;
     public string Username { get; set; } = default!;
     public string FullName { get; set; } = default!;
-    public string Locale { get; set; } = "et";
+    public string Locale { get; set; } = "en";
 }
